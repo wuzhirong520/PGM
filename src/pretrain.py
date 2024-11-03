c944fd1b61c4477bf996d88e41670d4e80c7bf00
wuzhirong520 <3154969194@qq.com>
2024-11-03 16:16:20 +0800

    24-11-03-001

--- src/pretrain.py
+++ src/pretrain.py
@@ -70,7 +70,7 @@ def evaluation(args, model, data_loader, device):
     return np.mean(eval_meter.compute_metric(DATASET_INFO[args.dataset]['metric']))
 
 
-def main(args, dataset, device):
+def main(args, dataset, run_name, device):
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
     if torch.cuda.is_available():
@@ -120,10 +120,10 @@ def main(args, dataset, device):
         progress_bar.set_postfix(**logs)
         wandb.log(logs)
 
-        if (epoch + 1) % 10 == 0:
-            save_dir = os.path.join(str(args.result_path), str(args.dataset))
+        if (epoch + 1) % args.save_ckpt_step == 0:
+            save_dir = os.path.join(str(args.log_path), run_name)
             os.makedirs(save_dir, exist_ok=True)
-            torch.save(model.state_dict(), os.path.join(save_dir, str(args.dataset) + f'_model_{epoch + 1}.pt'))
+            torch.save(model.state_dict(), os.path.join(save_dir, str(args.dataset) + f'_model_{epoch + 1:05}.pt'))
 
 
 if __name__ == '__main__':
@@ -133,24 +133,29 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dataset', type=str, default='BACE', choices=['MUV', 'BACE', 'BBBP', 'ClinTox', 'SIDER',
                                                     'ToxCast', 'HIV', 'PCBA', 'Tox21', 'FreeSolv', 'Lipophilicity', 'ESOL'],
                         help='Dataset to use')
-    parser.add_argument('--seed', type=int, default=0,
+    parser.add_argument('--seed', type=int, default=24012128,
                         help="Seed for minibatch selection, random initialization.")
     parser.add_argument('-s', '--split', type=str, default='scaffold', choices=['scaffold', 'random'],
                         help='Dataset splitting method (default: scaffold)')
     parser.add_argument('-sr', '--split-ratio', type=str, default='0.8,0.1,0.1',
                         help='Proportion of the dataset to use for training, validation and test (default: 0.8,0.1,0.1)')
-    parser.add_argument('-n', '--num-epochs', type=int, default=300,
-                        help='Maximum number of epochs for pre-training (default: 300)')
     parser.add_argument('-nw', '--num-workers', type=int, default=0,
                         help='Number of processes for data loading (default: 0)')
-    parser.add_argument('-rp', '--result_path', type=str, default='./log/pretrain')
+
+    parser.add_argument('-n', '--num-epochs', type=int, default=300,
+                        help='Maximum number of epochs for pre-training (default: 300)')
+    parser.add_argument('--log_path', type=str, default='./log')
+    parser.add_argument("-bs","--batch_size", type=int, default=4096)
+    parser.add_argument("--save_ckpt_step", type=int , default=10)
     args = parser.parse_args()
+
+    import time
+    run_name = "pretrain-"+args.dataset+"-"+str((int)(time.time()))
 
     device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
 
-    import time
-    wandb.init(project="PGM", name="pretrain-"+args.dataset+"-"+str((int)(time.time())),config=args)
+    wandb.init(project="PGM", name=run_name, config=args)
 
     dataset = load_dataset(args.dataset)
 
-    main(args, dataset, device)
+    main(args, dataset, run_name, device)
